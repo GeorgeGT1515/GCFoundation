b97a85cf016cff1132d50a757f322f81226cb356
Rainville, Yuli-Anne <YRAINVIL@tbs-sct.gc.ca>
2026-07-02 13:33:53 -0400

    added dynamic menu component thta would be shown by default by the foundation layout

---
+++ GCFoundation.Components/Services/Interfaces/ITopNavigationLocalizationService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCFoundation.Components.Services.Interfaces
+{
+    /// <summary>
+    /// Interface for a service that provides localization for navigation menu items.
+    /// </summary>
+    public interface ITopNavigationLocalizationService
+    {
+        /// <summary>
+        /// Gets the localized value for a given key.
+        /// </summary>
+        /// <param name="key">The key for which the localized value is to be retrieved.</param>
+        /// <returns>The localized value associated with the given key.</returns>
+        string GetLocalizeValue(string key);
+    }
+}
---
+++ GCFoundation.Components/Services/TopNavigationLocalizationService.cs
@@ -0,0 +1,26 @@
+﻿using GCFoundation.Components.Services.Interfaces;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCFoundation.Components.Services
+{
+    public class TopNavigationLocalizationService<T> : ITopNavigationLocalizationService
+    {
+        private readonly IStringLocalizer<T> _localizer;
+
+        public TopNavigationLocalizationService(IStringLocalizer<T> localizer)
+        {
+            _localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
+        }
+
+        public string GetLocalizeValue(string key)
+        {
+            var localizedString = _localizer[key];
+            return localizedString.Value;
+        }
+    }
+}
